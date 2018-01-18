7b478c7400683b58fbcbc8eb53f930ee2e261e48
vaishalivsb <31504057+vaishalivsb@users.noreply.github.com>
2018-01-18 11:10:44 +0000

    Update and rename hello.c to sum.c

--- hello.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main()
-{
-int s,i;
-scanf("%d",&s);
-for(i=1;i<s;i++)
-printf("Hello");
-return 0;
-}
---
+++ sum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+int a,i,sum=0;
+  scanf("%d",&a);
+  for(i=1;i<a;i++)
+    sum=sum+i;
+    printf("%d",sum);
+  return 0;
+}
